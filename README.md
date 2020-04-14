7eaeeead903a528b507fed98f74cf214c25fbfe0
Max der Entwickler <s0560245@htw-berlin.de>
2020-04-15 00:30:57 +0200

    modified license and copyright disclaimer, added class_diagram

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dogm128_driver_stm32
 This is a driver library to control the Electronics Assembly DOGM128 monochrome display with an STM32##
+
+## Installation
+
+## Copyright & copyright
+
+© Maximilian Weber
--- ea_dogm128_stm32_library/Src/bitmap_data.cpp
+++ ea_dogm128_stm32_library/Src/bitmap_data.cpp
@@ -1,22 +1,19 @@
 /*
- * This file is part of Maximilian Weber's Electronics Assembly DOGM128 Monochrome Display Library.
+ *    Copyright (C) 2019,2020  Maximilian Anton Weber
  *
- * See the COPYRIGHT file at the top-level directory of this distribution
- * for details of code ownership.
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "bitmap_data.hpp"
 
